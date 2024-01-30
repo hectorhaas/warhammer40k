bd37dbaf14b3ef96297681a2071a33e27f3b9f91
hector ayala solivan <hectorhaas@hotmail.com>
2024-01-30 00:37:21 -0600

    More changes

--- game.js
+++ game.js
@@ -3,15 +3,47 @@ let gameObj = {};
 class game_base_unit_model {
     name;
     keywords = [];
+    abilities = {};
+    auraAbilities = {};
     friendly = false;
 }
 
 class game_unit extends game_base_unit_model {
-    model_count = 0; // Minimum of 5
+    model_count_minimum = 0; // Minimum of 5
+    model_count_maximum = 0; // Minimum of 5
 }
 
 class game_model extends game_base_unit_model {
+    profiles = {
+        // These contain the following characteristics that tell you how mighty the models in the unit are.
+        move: NaN, // M
+        weaponSkill: NaN, // WS
+        ballisticSkill: NaN, // BS
+        strength: NaN, // S
+        toughness: NaN, // T
+        wounds: NaN, // W
+        attacks: NaN, // A
+        leadership: NaN, // Ld
+        save: NaN, // Sv
+    };
+    weapons = {};
+}
 
+class game_weapon {
+    id = NaN;
+    name;
+    range = {
+        minimum: NaN,
+        maximum: NaN
+    };
+    type;
+    strength = {
+        // If NaN, equal to user.
+        multiply: false,
+        points: NaN
+    }
+    armorPenetration = NaN;
+    damage = NaN;
 }
 
 $(document).ready(function (){
